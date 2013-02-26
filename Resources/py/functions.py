6718ca593e1d0a2f861a89efd9c2118b376ba7a5
Gabriel Limon <glimo002@ucr.edu>
2013-02-26 07:56:40 -0800

    first commit

---
+++ Resources/py/functions.py
@@ -0,0 +1,13 @@
+# A function needs to be written to take a matrix representing TEUs and boxes 
+# to be removed. The last array should be the buffer zone.
+# Said function needs to return a list of instructions represented as starting
+# positions and ending positions.
+def remove_boxes(manifest, desiredBoxPos):
+    return manifest, "stuff"
+
+def insert_box(manifest, label):
+    return manifest
+
+# each pos should be a pair of coordinates
+def move_box(manifest, startpos, endpos):
+    return manifest
